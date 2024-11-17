93ec14655b06af585f4792996c40878da6feace9
pastc <147841049+pastc@users.noreply.github.com>
2024-11-17 13:03:49 +0900

    Add btop configuration and theme files

--- link.sh
+++ link.sh
@@ -5,3 +5,6 @@ ln -sw $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
 rm -rf $HOME/.config/kitty
 ln -sw $HOME/.dotfiles/kitty $HOME/.config/kitty
+
+rm -rf $HOME/.config/btop
+ln -sw $HOME/.dotfiles/btop $HOME/.config/btop
